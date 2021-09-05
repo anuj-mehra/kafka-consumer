5c18bb6156119a0d1e8c8737ae0410f04e9e7a86
anuj-mehra <mehra.anuj1712@gmail.com>
2021-09-05 21:46:28 +0530

    refactored application.conf

--- src/main/scala/com/poc/kafkaconsumer/config/KafkaConsumerConfig.scala
+++ src/main/scala/com/poc/kafkaconsumer/config/KafkaConsumerConfig.scala
@@ -12,17 +12,22 @@ class KafkaConsumerConfig(applicationConfFile: String) extends Serializable {
   ssl.truststore.location=""
   ssl.truststore.password=""
   service.discovery.url = ""*/
+  /* security.protocol= ""
+   sasl.mechanism = ""
+   kafka.username=""
+   kafka.password=""*/
 
-  lazy val bootstrapServers= config.getString("consumer_config.json_message.bootstrap_servers")
+  lazy val bootstrapServers= config.getString("consumer_config.bootstrap_servers")
   lazy val jsonMsgConsumerGroupId = config.getString("consumer_config.json_message.consumer_group_id")
   lazy val jsonMsgTopicName = config.getString("consumer_config.json_message.topic_name")
   lazy val jsonMsgKeySerializer = config.getString("consumer_config.json_message.key_serializer")
   lazy val jsonMsgValueSerializer = config.getString("consumer_config.json_message.value_serializer")
 
- /* security.protocol= ""
-  sasl.mechanism = ""
-  kafka.username=""
-  kafka.password=""*/
+  lazy val avroMsgConsumerGroupId = config.getString("consumer_config.avro_message.consumer_group_id")
+  lazy val avroMsgTopicName = config.getString("consumer_config.avro_message.topic_name")
+  lazy val avroMsgKeySerializer = config.getString("consumer_config.avro_message.key_serializer")
+  lazy val avroMsgValueSerializer = config.getString("consumer_config.avro_message.value_serializer")
+
 }
 
 object KafkaConsumerConfig {
